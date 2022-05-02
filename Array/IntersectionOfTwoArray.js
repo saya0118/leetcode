db105d12c18ec90dbab6b4be23962b10d4a1c54e
saya0118 <chemi0802@gmail.com>
2022-05-01 23:57:04 -0700

    resolved"

---
+++ Array/IntersectionOfTwoArray.js
@@ -0,0 +1,20 @@
+var intersect = function(nums1, nums2) {
+    let obj = {};
+    let result = [];
+
+    for(let i of nums1){
+        if (!obj[i]){
+            obj[i] = 1
+        }else {
+            obj[i]++
+        }
+    }
+
+    for (let i of nums2){
+        if(obj[i] > 0){
+            result.push(i)
+            obj[i]--
+        }
+    }
+    return(result)
+}
