7e7e251b842605c64eb2930680e1b66475d5f006
saya0118 <chemi0802@gmail.com>
2022-06-01 20:04:13 -0700

    solved

---
+++ Math/FizzBuzz.js
@@ -0,0 +1,16 @@
+var fizzBuzz = function (n) {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 3 === 0 && i % 5 !== 0) {
+      arr.push("Fizz");
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      arr.push("Buzz");
+    } else {
+      let string = i.toString();
+      arr.push(string);
+    }
+  }
+  return arr;
+};
